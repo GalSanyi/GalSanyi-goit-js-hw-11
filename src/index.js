e70a1003ce5941b17e3b7fc0959786db96203875
GalSanyi <galsanyi84@gmail.com>
2021-12-30 12:21:36 +0100

    new add

--- src/index.js
+++ src/index.js
@@ -3,41 +3,26 @@ import axios from 'axios';
 import fotoOneCard from '../tamplates/fotoOneCard.hbs';
 import './sass/main.scss';
 
+//
+let searchQuery = '';
 const refs = {
     searchForm: document.querySelector('.search-form'),
     gallery: document.querySelector('.gallery'),
-    LoadMore: document.querySelector('.load-more'),
+    loadMore: document.querySelector('.load-more'),
 }
-
-// const BASE_URL = 'https://pixabay.com/api';
-
-// function fetchPixabay() {
-//     return fetch(`${BASE_URL}/?key=25003367-734d14b32e98f9c9fd7c27c2f&q=yellow&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`).then(response => response.json()).then(console.log);
-
-// }
-
-const fetchPixabay = () => {
-    return axios.get(`https://pixabay.com/api/?key=25003367-734d14b32e98f9c9fd7c27c2f&q="&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`);
-};
-
-fetchPixabay({
-    q: '',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-
-}).then(data => console.log(data));;
-
-
-
-
 refs.searchForm.addEventListener('submit', onSearchForm);
+refs.loadMore.addEventListener('click', onLoadMore);
 
 function onSearchForm(evt) {
     evt.preventDefault();
-    const form = evt.currentTarget.value;
-    const value = form.trim();
-    console.log(value);
-    console.log(object);
+    searchQuery = evt.currentTarget.elements.searchQuery.value;
+    fetch(`https://pixabay.com/api/?key=25003367-734d14b32e98f9c9fd7c27c2f&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`).then(res => res.json()).then(console.log);
+
+}
+
+
+function onLoadMore() {
+    fetch(`https://pixabay.com/api/?key=25003367-734d14b32e98f9c9fd7c27c2f&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1`).then(res => res.json()).then(console.log);
+
 
 }
