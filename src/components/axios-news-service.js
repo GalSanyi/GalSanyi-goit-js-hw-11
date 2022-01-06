ef346e7a8f40c6b50d44d490408ac76baa4cdc4f
GalSanyi <galsanyi84@gmail.com>
2022-01-06 21:04:37 +0100

    gut

--- src/components/axios-news-service.js
+++ src/components/axios-news-service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
-const API_KEY = '25003367-734d14b32e98f9c9fd7c27c2f';
-const BASE_URL = 'https://pixabay.com/api';
+// const API_KEY = '25003367-734d14b32e98f9c9fd7c27c2f';
+// const BASE_URL = 'https://pixabay.com/api';
 export default class NewsApiService {
     constructor() {
         //глобальная переманная для поиска
@@ -8,15 +8,20 @@ export default class NewsApiService {
         this.page = 1;
     }
     async fetchArticles() {
-            console.log(this);
-            return fetch(`${BASE_URL}/?key=${API_KEY}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`)
-                .then(response => response.json())
-                .then(({ hits }) => {
-                    console.log(hits);
-                    //если результат успешный страницу увеличиваем на 1
-                    this.incrementPage();
-                    return hits;
-                });
+            this.incrementPage();
+            return axios({
+                method: 'get',
+                url: 'https://pixabay.com/api/',
+                params: {
+                    key: '24592652-81dd428b6cc1f580195381066',
+                    q: `${this.searchQuery}`,
+                    page: `${this.page}`,
+                    image_type: 'photo',
+                    orientation: 'horizontal',
+                    safesearch: 'true',
+                    per_page: 40,
+                },
+            });
         }
         //метод увеличения страницы
     incrementPage() {
@@ -32,4 +37,35 @@ export default class NewsApiService {
     set query(newQuery) {
         this.searchQuery = newQuery;
     }
+
+
+    // constructor() {
+    //     this.page = 1;
+    //     this.searchValue = '';
+    //   }
+
+    //   incrementPage() {
+    //     this.page += 1;
+    //   }
+
+    //   resetPage() {
+    //     this.page = 1;
+    //   }
+
+    //   async fetch() {
+    //     this.incrementPage();
+    //     return axios({
+    //       method: 'get',
+    //       url: 'https://pixabay.com/api/',
+    //       params: {
+    //         key: '24592652-81dd428b6cc1f580195381066',
+    //         q: `${this.searchValue}`,
+    //         page: `${this.page}`,
+    //         image_type: 'photo',
+    //         orientation: 'horizontal',
+    //         safesearch: 'true',
+    //         per_page: 40,
+    //       },
+    //     });
+    //   }
 }
